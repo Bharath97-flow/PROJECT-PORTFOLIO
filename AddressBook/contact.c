fb70aec59b6c3655e18be89a6caf4a7e6b4c0b20
quantum.1 <113760331+Bharath97-flow@users.noreply.github.com>
2025-11-28 09:15:54 +0530

    Update contact.c

--- AddressBook/contact.c
+++ AddressBook/contact.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "contact.h"
 #include "file.h"
 #include "populate.h"
@@ -860,5 +859,6 @@ void deleteContact(AddressBook *addressBook)
         }
     }
 }
+
 
            
